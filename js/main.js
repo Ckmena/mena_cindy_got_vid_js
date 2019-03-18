b175bfefb06ea5ab3967c57ba94344f6b63529c4
Ckmena <c_menavalencia@fanshaweonline.ca>
2019-03-18 12:02:31 -0400

    targeting class and adding video

--- js/main.js
+++ js/main.js
@@ -6,12 +6,34 @@
 	video = document.querySelector('video');
 
 	function openLightbox() {
+		//debugger;
+		let targetHouse = this.className.split(" ")[1];
+
+		//this give us back a lowercase house name -> the second class on all of the shield ie stark , baratheon
+		//flip this to upercase
+		targetHouse = targetHouse.replace(targetHouse.chartAt(0),
+			targetHouse.splice(1));
+
+
+		let targetSource = `video/House-${targetHouse}.mp4`;
+
 		lightbox.classList.add('lightbox-on');
+		video.load();
+		video.play();
 	}
 
 	function closeLightbox() {
 		lightbox.classList.remove('lightbox-on');
+
+		//rewind the current video and pause it
+		video.currentTime = 0;
+		video.pause();
+
 	}
+
+	
+
+
 
 	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
 
