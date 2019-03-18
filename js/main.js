56259c67c3d3e33a2cd7588e990aa724f6dd37cf
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-03-18 12:28:54 -0400

    added different videos and functions

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 (() => {
 	console.log('JS linked');
 
-	const sigils = document.querySelectorAll('.sigilContainer'),
-	lightbox = document.querySelector('.lightbox'),
-	video = document.querySelector('video');
+	const sigils	= document.querySelectorAll('.sigilContainer'),
+	lightbox 		= document.querySelector('.lightbox'),
+	video 			= document.querySelector('video'),
+	closeButton		= document.querySelector('.lightbox-close');	
 
 	function openLightbox() {
+		// debugger;
+		let targetHouse = this.className.split(" ")[1];
+		//this gives us a class for house name. this makes a 2nd class ont he shields.
+		
+		//flipping source to uppercase
+		//let targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetHouse}.mp4`; 
+		// I changed the src files to lowercase. if i did not i need to include the code above and chage video.src to 'targetVid'
 		lightbox.classList.add('lightbox-on');
+
+		video.load();
 		video.play();
 		
 	}
@@ -20,6 +32,6 @@
 	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
 
 	video.addEventListener('ended', closeLightbox);
-
+	closeButton.addEventListener('click', closeLightbox);
 
 })();
