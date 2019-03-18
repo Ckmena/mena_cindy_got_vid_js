8ff8a2c96f1493e081a5b624a870fedfbc2355c7
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-03-18 11:29:14 -0400

    bug fixes in js and css

--- js/main.js
+++ js/main.js
@@ -7,10 +7,14 @@
 
 	function openLightbox() {
 		lightbox.classList.add('lightbox-on');
+		video.play();
+		
 	}
 
 	function closeLightbox() {
 		lightbox.classList.remove('lightbox-on');
+		video.currentTime = 0;
+		video.pause();
 	}
 
 	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
